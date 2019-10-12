98320fa893d66ec94cdf121450f500c95087314d
Deddy Utomo <deddy.utomo@mandiriinhealth.local>
2019-10-12 11:43:10 +0700

    modified orders table

--- database/migrations/2019_10_01_000010_create_orders_table.php
+++ database/migrations/2019_10_01_000010_create_orders_table.php
@@ -15,6 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->double('item_price', 8, 2);
             $table->timestamps();
         });
     }
