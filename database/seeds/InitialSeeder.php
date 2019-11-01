64a917260cccf1a3b46622f8d619bfbe8deaa731
Deddy Utomo <deddy.utomo@mandiriinhealth.local>
2019-11-01 21:57:25 +0700

    aa

--- database/seeds/InitialSeeder.php
+++ database/seeds/InitialSeeder.php
@@ -62,38 +62,6 @@ class InitialSeeder extends Seeder
             'id' => 1, 'code' => 'HTL-01', 'name' => 'Hotel Jakarta', 'location' => 'Jakarta', 'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget elit in est faucibus semper sit amet mattis nulla. Praesent eget congue enim. Pellentesque volutpat.'
         ]);
 
-<<<<<<< HEAD
-        /* INSERT DATA TO PACKAGES TABLE */
-        DB::table('packages')->insert([
-            'id' => 1, 'code' => 'P001', 'name' => 'Paket Rekreasi Jakarta', 'default_picture' => 'images/paket_rekreasi_jakarta.jpg', 'desc' => 'Dunia Fantasi adalah salah satu tempat rekresi yang tepat untuk wahana bermain dan berkumpul bersama keluarga atau teman.', 'location' => 'Jakarta', 'price' => '300000'
-        ]);
-        DB::table('packages')->insert([
-            'id' => 2, 'code' => 'P002', 'name' => 'Paket Wisata Lembang', 'default_picture' => 'images/paket_wisata_lembang.jpg', 'desc' => 'Lembang merupakan objek wisata dimana kamu bisa belajar sejarah di tengah-tengah modernisasi Kota Jakarta.', 'location' => 'Bandung', 'price' => '500000'
-        ]);
-        DB::table('packages')->insert([
-            'id' => 3, 'code' => 'P003', 'name' => 'Paket Kepulauan Seribu', 'default_picture' => 'images/paket_kepulauan_seribu.jpg', 'desc' => 'Kepulauan seribu merupakan objek wisata dimana kamu bisa belajar sejarah di tengah-tengah modernisasi Kota Jakarta.', 'location' => 'Jakarta', 'price' => '400000'
-        ]);
-        DB::table('packages')->insert([
-            'id' => 4, 'code' => 'P004', 'name' => 'Paket Wisata Bromo', 'default_picture' => 'images/paket_bromo.jpg', 'desc' => 'Dunia Fantasi adalah salah satu tempat rekresi yang tepat untuk wahana bermain dan berkumpul bersama keluarga atau teman.', 'location' => 'Malang', 'price' => '500000'
-        ]);
-        DB::table('packages')->insert([
-            'id' => 5, 'code' => 'P005', 'name' => 'Paket Wisata Jogja', 'default_picture' => 'images/paket_jogja.jpg', 'desc' => 'Lembang merupakan objek wisata dimana kamu bisa belajar sejarah di tengah-tengah modernisasi Kota Jakarta.', 'location' => 'Jogjakarta', 'price' => '400000'
-        ]);
-        DB::table('packages')->insert([
-            'id' => 6, 'code' => 'P006', 'name' => 'Paket Wisata Surabaya', 'default_picture' => 'images/paket_surabaya.png', 'desc' => 'Kepulauan seribu merupakan objek wisata dimana kamu bisa belajar sejarah di tengah-tengah modernisasi Kota Jakarta.', 'location' => 'Surabaya', 'price' => '400000'
-        ]);
-
-
-        /* INSERT DATA TO TOURISM_SITES TABLE */
-        DB::table('tourism_sites')->insert([
-            'id' => 1, 'code' => 'TS001', 'name' => 'Dunia Fantasi', 'desc' => 'Dunia Fantasi adalah salah satu tempat rekresi yang tepat untuk wahana bermain dan berkumpul bersama keluarga atau teman.', 'location' => 'Jakarta', 'price' => '125000'
-        ]);
-        DB::table('tourism_sites')->insert([
-            'id' => 2, 'code' => 'TS002', 'name' => 'Kota Tua', 'desc' => 'Kota Tua merupakan objek wisata dimana kamu bisa belajar sejarah di tengah-tengah modernisasi Kota Jakarta.', 'location' => 'Jakarta', 'price' => '5000'
-        ]);
-        DB::table('tourism_sites')->insert([
-            'id' => 3, 'code' => 'TS003', 'name' => 'Farm House Lembang', 'desc' => 'Farm House Lembang merupakan objek wisata dimana kamu bisa belajar sejarah di tengah-tengah modernisasi Kota Jakarta.', 'location' => 'Bandung', 'price' => '30000'
-=======
         /* DELETE DATA TO TOURISM_SITES TABLE */
         DB::table('tourism_sites')->delete();
         /* INSERT DATA TO TOURISM_SITES TABLE */
@@ -141,7 +109,6 @@ class InitialSeeder extends Seeder
         ]);
         DB::table('tourism_sites')->insert([
             'id' => 15, 'code' => 'TS015', 'name' => 'Depok Fantasi', 'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget elit in est faucibus semper sit amet mattis nulla. Praesent eget congue enim. Pellentesque volutpat.', 'location' => 'jawa barat', 'price' => '30000', 'default_picture' => 'uploads/wisata/05_jawa_barat/depok_fantasi.jpg'
->>>>>>> 779fb8617195292f90ee91ef04981164560a92e8
         ]);
 
 
@@ -156,8 +123,6 @@ class InitialSeeder extends Seeder
             'category_id' => 5, 'tourism_site_id' => '3'
         ]);
 
-<<<<<<< HEAD
-=======
         /* DELETE DATA TO TOURISM_SITES TABLE */
         DB::table('packages')->delete();
         /* INSERT DATA TO PACKAGES TABLE */
@@ -170,6 +135,5 @@ class InitialSeeder extends Seeder
         DB::table('packages')->insert([
             'id' => 3, 'code' => 'P003', 'name' => 'Paket Wisata Kepulauan Seribu', 'desc' => 'Kepulauan seribu merupakan objek wisata dimana kamu bisa belajar sejarah di tengah-tengah modernisasi Kota Jakarta.', 'location' => 'Jakarta', 'price' => '400000', 'default_picture' => 'uploads/wisata/05_jawa_barat/kota_tua.jpg'
         ]);
->>>>>>> 779fb8617195292f90ee91ef04981164560a92e8
     }
 }
