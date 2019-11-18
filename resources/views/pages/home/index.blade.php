ad4cfc498bc9bd4285966dd4050d9e6f72b8b074
Novsha <nop.damanik@gmail.com>
2019-11-18 21:34:12 +0700

    Update fitur komentar/review

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -487,13 +487,13 @@ DTPL Kelompok 6
             </div>
             <div class="col-lg-7">
                 <div class="contact_form_container">
-                    <div class="contact_title">Yuk, kontak kita</div>
+                    <div class="contact_title">Yuk, Kasi Komentar :)</div>
                     <form action="#" id="contact_form" class="contact_form">
-                        <input type="text" id="contact_form_name" class="contact_form_name input_field" placeholder="Name" required="required" data-error="Name is required.">
+                        <input type="text" id="contact_form_name" class="contact_form_name input_field" placeholder="Nama" required="required" data-error="Name is required.">
                         <input type="text" id="contact_form_email" class="contact_form_email input_field" placeholder="E-mail" required="required" data-error="Email is required.">
-                        <input type="text" id="contact_form_subject" class="contact_form_subject input_field" placeholder="Subject" required="required" data-error="Subject is required.">
-                        <textarea id="contact_form_message" class="text_field contact_form_message" name="message" rows="4" placeholder="Message" required="required" data-error="Please, write us a message."></textarea>
-                        <button type="submit" id="form_submit_button" class="form_submit_button button">send message<span></span><span></span><span></span></button>
+                        <!-- <input type="text" id="contact_form_subject" class="contact_form_subject input_field" placeholder="Subject" required="required" data-error="Subject is required."> -->
+                        <textarea id="contact_form_message" class="text_field contact_form_message" name="message" rows="4" placeholder="Komentar" required="required" data-error="Please, write us a message."></textarea>
+                        <button type="submit" id="form_submit_button" class="form_submit_button button">Kirim Komentar<span></span><span></span><span></span></button>
                     </form>
                 </div>
             </div>
